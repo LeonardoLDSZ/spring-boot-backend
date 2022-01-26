7d742a10043c9cc06e5e0737525003b2a2a31680
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-01-25 21:23:22 -0300

    Criado operação de instanciação

--- src/main/java/com/leonardodsz/CourseApplication.java
+++ src/main/java/com/leonardodsz/CourseApplication.java
@@ -1,13 +1,32 @@
 package com.leonardodsz;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leonardodsz.domain.Categoria;
+import com.leonardodsz.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CourseApplication {
+public class CourseApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+	}
+	
+	@Override
+	public void run(String...args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática"); //Objetos categoria criados
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
