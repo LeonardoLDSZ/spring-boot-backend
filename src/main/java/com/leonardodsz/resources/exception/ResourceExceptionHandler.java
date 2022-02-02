5b4452c808aa9f2b00d374daf45d480b3762ccae
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-02-02 07:12:30 -0300

    Delete - Categoria

--- src/main/java/com/leonardodsz/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/leonardodsz/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.leonardodsz.services.exceptions.DataIntegrityException;
 import com.leonardodsz.services.exceptions.ObjectNotFoundException;
 
 
@@ -18,5 +19,11 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/leonardodsz/services/CategoriaService.java
+++ src/main/java/com/leonardodsz/services/CategoriaService.java
@@ -3,9 +3,12 @@ package com.leonardodsz.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
 import com.leonardodsz.domain.Categoria;
 import com.leonardodsz.repositories.CategoriaRepository;
+import com.leonardodsz.services.exceptions.DataIntegrityException;
 import com.leonardodsz.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,14 +23,23 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 	}		
 
-public Categoria insert(Categoria obj ) {
-	obj.setId(null);
-	return repo.save(obj);
+	public Categoria insert(Categoria obj ) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
-public Categoria update(Categoria obj) {
-	find(obj.getId());
-	return repo.save(obj);
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
 }
