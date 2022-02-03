510aed094a8dafd22169dd8e2c7962e2d134a203
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-02-02 22:09:26 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/leonardodsz/services/CategoriaService.java
+++ src/main/java/com/leonardodsz/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leonardodsz.domain.Categoria;
+import com.leonardodsz.dto.CategoriaDTO;
 import com.leonardodsz.repositories.CategoriaRepository;
 import com.leonardodsz.services.exceptions.DataIntegrityException;
 import com.leonardodsz.services.exceptions.ObjectNotFoundException;
@@ -53,7 +54,10 @@ public class CategoriaService {
 	
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
-		return repo.findAll(pageRequest);
-		
+		return repo.findAll(pageRequest);		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
