9e67928fcf6e012af4a258ee72291a56815354ba
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-02-23 21:10:33 -0300

    ProdutoDTO-ProdutoResource-ProdutoService

--- src/main/java/com/leonardodsz/repositories/ProdutoRepository.java
+++ src/main/java/com/leonardodsz/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.leonardodsz.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.leonardodsz.domain.Categoria;
 import com.leonardodsz.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 				//Este objeto realizará acesso a dados e operações, salvar, deletar, alterar
 				//Camada de acesso a dados do tipo categoria
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.name LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
